e6a4cbe9a561468d2b5240099f19b14b1437c219
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-01-07 19:54:34 +0330

    Organize Program.cs file

--- src/SAF.CasualTimes.Api/Program.cs
+++ src/SAF.CasualTimes.Api/Program.cs
@@ -1,17 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+{
+	builder.Services.AddControllers();
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-app.UseHttpsRedirection();
-app.MapControllers();
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+	app.UseHttpsRedirection();
+	app.MapControllers();
 
-app.Run();
+	app.Run();
+}
